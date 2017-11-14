49ff4ccd2dc11a5e963aa6020051c9bf3bf4730d
Aaron Abraham <aaronabraham@Macintosh-3.local>
2017-11-14 09:34:56 -0700

    Deleted files

--- src/data-combine.py
+++ src/data-combine.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import numpy as np
 
 """
 Merge datasets: https://pandas.pydata.org/pandas-docs/stable/merging.html
 """
 
 # Reading in csv files and cleaning 
-alberta = pd.read_csv("../data/raw/Alberta Disaster Data.csv");
-cleanAlberta = alberta.dropna()
+alberta = pd.read_csv("../data/raw/Alberta Disaster Data.csv", sep = '\t');
+cleanAlberta = alberta[alberta["Fatalities"] != "Unknown"]
 
 bc = pd.read_csv("../data/raw/BC Disaster Data.csv")
 cleanBC = bc.dropna()
@@ -14,19 +15,19 @@ cleanBC = bc.dropna()
 international = pd.read_csv("../data/raw/International Disaster Data.csv")
 cleanInter = international.dropna()
 
-manitoba = pd.read_csv("../data/raw/Manitoba Disaster Data.csv")
+manitoba = pd.read_csv("../data/raw/Manitoba Disaster Data.csv", sep = '\t')
 cleanManitoba = manitoba.dropna()
 
-NB = pd.read_csv("../data/raw/New Brunswick Disaster Data.csv")
+NB = pd.read_csv("../data/raw/New Brunswick Disaster Data.csv", sep = '\t')
 cleanNB = NB.dropna()
 
-NL = pd.read_csv("../data/raw/Newfoundland and Labrador Disaster Data.csv")
+NL = pd.read_csv("../data/raw/Newfoundland and Labrador Disaster Data.csv", sep = '\t')
 cleanNL = NL.dropna()
 
-NWT = pd.read_csv("../data/raw/Northwest Territories Disaster Data.csv")
+NWT = pd.read_csv("../data/raw/Northwest Territories Disaster Data.csv", sep = '\t')
 cleanNWT = NWT.dropna()
 
-NS = pd.read_csv("../data/raw/Nova Scotia Disaster Data.csv")
+NS = pd.read_csv("../data/raw/Nova Scotia Disaster Data.csv", sep = '\t')
 cleanNS = NS.dropna()
 
 nunavut = pd.read_csv ("../data/raw/Nunavut Disaster Data.csv")
