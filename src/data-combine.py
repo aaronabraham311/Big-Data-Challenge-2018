19bdc8c7f613aac8a19d6bc5a0abd2b20babb47f
Aaron Abraham <aaronabraham@Macintosh-3.local>
2017-12-23 12:52:20 -0700

    cleaning and visualizations

--- src/data-combine.py
+++ src/data-combine.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+"""def violinVisualization (df):
+    for i in len(df.columns):
+        plot = sns.violinplot(df[:,i])
+        print(plot) """
+
 # Reading in csv files and cleaning 
 df = pd.read_csv("../data/raw/glaciers.csv")
 
@@ -21,10 +26,41 @@ df = df.dropna()
 # Drop outliers?
 df.info() # Every column aside from Glacier ID --> Glacier Name is a float. Boxplots or violin to see variance
 
+#violinVisualization(df)
+
+## Outlier detection using violin plots. All have bimodal distributions
+#sns.violinplot(x = df["Minimum Elevation"]) # Insights: Values below 0 can be dropped
+#sns.violinplot(x = df["Minimum Elevation Exposed"]) # Same as above
+#sns.violinplot(x = df["Maximum Elevation"]) # No outliers
+#sns.violinplot(x = df["Mean Elevation"]) # No outliers
+#sns.violinplot(x = df["Maximum Elevation"]) # No outliers
+#sns.violinplot(x = df["Snow Line Elevation"]) # Drop everything below 2000
+#sns.violinplot(x = df["Snow Line Accuracy"]) # Drop accuracy 
+#sns.violinplot(x = df["Area Accuracy"]) # Skipped Glacier Area as that is the variable we are looking at
+#sns.violinplot(x = df["Mean Width"]) # Drop everything above 10
+#sns.violinplot(x = df["Mean Length"]) # Drop everything above 35
+#sns.violinplot(x = df["Maximum Length"]) # Drop everything above 40
+#sns.violinplot(x = df["Maximum Length Exposed"]) # Same as above
+#sns.violinplot(x = df["Mean Depth"]) # Drop everything above 300
+#sns.violinplot(x = df["Depth Accuracy"])# Drop accuracy """
+### The rest of the variables are not needed for outlier detection. They are either numbers or objects
+
+
+#Outlier drop
+
+df= df[df["Minimum Elevation"] >= 0 ]
+df = df[df["Minimum Elevation Exposed"] >= 0]
+df = df[df["Snow Line Elevation"] >= 2000]
+df = df.drop("Snow Line Accuracy", 1)
+df = df.drop("Area Accuracy", 1)
+df = df.drop("Depth Accuracy", 1)
+df = df[df["Mean Width"] <= 10]
+df = df[df["Mean Length"] <= 35]
+df = df[df["Maximum Length"] <= 40]
+df = df[df["Maximum Length Exposed"] <= 40]
+df = df[df["Mean Depth"] <= 300]
+
+#Rows left: 610
+
 # Writing csv
 df.to_csv("../data/processed/cleaned_glaciers.csv")
-
-## Outlier detection using violin plots
-min_el_vp = sns.violinplot(x = df["Minimum Elevation"]) # Insights: Values below 0 can be dropped
-min_el_ex_vp = sns.violinplot(x = df["Minimum Elevation Exposed"]) # Same as above
-max_el_vp = sns.violinplot(x = df["Maximum Elevation"])
